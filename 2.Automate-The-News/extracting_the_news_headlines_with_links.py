d60b7b145514c82f53ae201ae69a1a7abe0f1859
Sana <SanaAlmatrafi@gmail.com>
2026-06-16 09:47:49 +0300

    Add file extracting_the_news_headlines_with_links

---
+++ 2.Automate-The-News/extracting_the_news_headlines_with_links.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+
+web = 'https://www.thesun.co.uk/sport/football/'
+path = '/home/sana/repos/Learning-Automation-with-Python/2.Automate-The-News/chromedriver-linux64/chromedriver'  # introduce path here
+
+# Creating the driver
+driver_service = Service(executable_path=path)
+driver = webdriver.Chrome(service=driver_service)
+driver.command_executor.set_timeout(1000)
+driver.get(web)
+
+#//div[@class="story__copy-container"]
+# Finding Elements
+containers = driver.find_elements(by='xpath', value='//div[@class="story__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+for container in containers:
+    title = container.find_element(by='xpath', value='./a/p').text
+    subtitle = container.find_element(by='xpath', value='./a/h3').text
+    link = container.find_element(by='xpath', value='./a').get_attribute('href')
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+# Exporting data to a CSV file
+my_dictionary = {'title': titles, 'subtitle': subtitles, 'link': links}
+df_headlines = pd.DataFrame(my_dictionary)
+df_headlines.to_csv('headlines.csv')
+
+driver.quit()
